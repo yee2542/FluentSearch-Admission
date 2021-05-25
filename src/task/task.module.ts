685092c2f89fe3d073c951bc5bb097b6d4ea45f0
yee2542 <yee2542gtl@gmail.com>
2021-05-26 01:11:53 +0700

    register taskschema

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import taskSchema, {
+  TASKS_SCHEMA_NAME,
+} from 'fluentsearch-types/dist/entity/task.entity';
 import { TaskService } from './task.service';
 
 @Module({
-  providers: [TaskService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: TASKS_SCHEMA_NAME, schema: taskSchema },
+    ]),
+  ],
+  providers: [TaskService],
 })
 export class TaskModule {}
