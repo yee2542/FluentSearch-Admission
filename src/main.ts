7357b86f9873d77c0ddc98e56708cd56c15bdde1
yee2542 <yee2542gtl@gmail.com>
2021-05-25 07:06:16 +0700

    implement config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ConfigAppProviderType } from './config/@types/config-app.type';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config: ConfigAppProviderType = app
+    .select(ConfigModule)
+    .get(ConfigService)
+    .get();
+
+  app.enableCors({
+    origin: [config.origin],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  await app.listen(config.port);
 }
 bootstrap();
