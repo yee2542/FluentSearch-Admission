383f84f0ca7997d98c17fdebc03fac1ac14cc0bd
yee2542 <yee2542gtl@gmail.com>
2021-05-26 01:01:22 +0700

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigDatabaseService } from './config/config.database.service';
 import { ConfigModule } from './config/config.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [ConfigModule, TaskModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: ConfigDatabaseService,
+    }),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
