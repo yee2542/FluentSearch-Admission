31efa235c4d7e1f62715ccc141019ef9d29823a7
yee2542 <yee2542gtl@gmail.com>
2021-05-25 08:41:45 +0700

    add simple queue listinening

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import amqplib from 'amqplib';
+import { ConfigService } from './config/config.service';
+import { ADMISSION_QUEUE } from 'fluentsearch-types';
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {}
+
+  async onModuleInit() {
+    const config = this.configService.get().rabbitmq;
+    const connectionString = `amqp://${config.username}:${config.password}@${config.endpoint}:5672`;
+    const mq = await amqplib.connect(connectionString);
+    const channel = await mq.createChannel();
+
+    channel.consume(ADMISSION_QUEUE, (msg) => {
+      const payload = JSON.parse(msg?.content.toString() || '');
+      console.log(payload);
+
+      msg && channel.ack(msg);
+    });
   }
 }
