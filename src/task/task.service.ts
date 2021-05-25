cf675a2f92faff3a871fd15aaa0e10ca4e454efc
yee2542 <yee2542gtl@gmail.com>
2021-05-26 02:17:40 +0700

    add create mthod

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { TaskDocument, TaskDTO, TASKS_SCHEMA_NAME } from 'fluentsearch-types';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @InjectModel(TASKS_SCHEMA_NAME)
+    private readonly taskMdel: Model<TaskDocument>,
+  ) {}
+
+  async createTask(task: TaskDTO) {
+    return this.taskMdel.create(task);
+  }
+}
