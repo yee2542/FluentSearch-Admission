cbd82e4e88fb1574bfce370f7e74c43dea146e99
yee2542 <yee2542gtl@gmail.com>
2021-05-27 00:58:40 +0700

    import need deps

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import taskSchema, {
   TASKS_SCHEMA_NAME,
 } from 'fluentsearch-types/dist/entity/task.entity';
+import { QuotaModule } from 'src/quota/quota.module';
 import { TaskResolver } from './task.resolver';
 import { TaskService } from './task.service';
 const TaskInstance = MongooseModule.forFeature([
   { name: TASKS_SCHEMA_NAME, schema: taskSchema },
 ]);
 @Module({
-  imports: [TaskInstance],
+  imports: [TaskInstance, QuotaModule],
   providers: [TaskService, TaskResolver],
   exports: [TaskService, TaskInstance],
 })
