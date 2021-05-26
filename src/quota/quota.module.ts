abecb0c6ceea802299dfeaba8eac01af28c8f834
yee2542 <yee2542gtl@gmail.com>
2021-05-26 07:19:04 +0700

    add qupta serivce

--- src/quota/quota.module.ts
+++ src/quota/quota.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QUOTAS_SCHEMA_NAME } from 'fluentsearch-types';
+import quotaSchema from 'fluentsearch-types/dist/entity/quota.entity';
 import { QuotaService } from './quota.service';
-
+const QuotaInstance = MongooseModule.forFeature([
+  { name: QUOTAS_SCHEMA_NAME, schema: quotaSchema },
+]);
 @Module({
+  imports: [QuotaInstance],
   providers: [QuotaService],
+  exports: [QuotaInstance, QuotaService],
 })
 export class QuotaModule {}
--- src/quota/quota.service.ts
+++ src/quota/quota.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { QuotaDocument, QUOTAS_SCHEMA_NAME } from 'fluentsearch-types';
+import { Model } from 'mongoose';
 
+export const DEFAULT_QUOTA = 20;
 @Injectable()
-export class QuotaService {}
+export class QuotaService {
+  constructor(
+    @InjectModel(QUOTAS_SCHEMA_NAME)
+    private readonly quotaModel: Model<QuotaDocument>,
+  ) {}
+
+  async upsertQuota(owner: string) {
+    const quota = await this.quotaModel.findOne({ owner });
+    if (quota) return quota;
+    return this.quotaModel.create({ owner, available: DEFAULT_QUOTA });
+  }
+
+  async getUserQuota(owner: string) {
+    return this.quotaModel.findOne({ owner }).lean();
+  }
+}
