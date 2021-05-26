7e798cda2d7094b62fd6bf23c136a09d1a37ba95
yee2542 <yee2542gtl@gmail.com>
2021-05-26 20:53:29 +0700

    add set exec, penalty

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import {
+  QUOTAS_SCHEMA_NAME,
   TaskDocument,
   TaskDTO,
   TaskStateEnum,
   TASKS_SCHEMA_NAME,
 } from 'fluentsearch-types';
 import { Model } from 'mongoose';
+
+export const LIMIT_WATCH_DOG_TASK = 10;
 
 @Injectable()
 export class TaskService {
@@ -18,4 +21,55 @@ export class TaskService {
   async queueTask(task: TaskDTO) {
     return this.taskMdel.create({ ...task, state: TaskStateEnum.wait });
   }
+
+  async getWaitQueue() {
+    return this.taskMdel
+      .find({ state: TaskStateEnum.wait })
+      .sort({ createAt: 1, priority: 1, updateAt: -1 })
+      .limit(LIMIT_WATCH_DOG_TASK);
+  }
+
+  async setPenaltyTask(task: TaskDocument) {
+    task.priority += 1;
+    task.updateAt = new Date();
+    return task.save();
+  }
+
+  async setExcuteTask(task: TaskDocument) {
+    task.state = TaskStateEnum.excute;
+    task.updateAt = new Date();
+    return task.save();
+  }
+
+  // async getReadyToSendInsight() {
+  //   return this.taskMdel.aggregate([
+  //     {
+  //       $match: {
+  //         type: TaskStateEnum.wait,
+  //       },
+  //     },
+  //     {
+  //       $lookup: {
+  //         from: QUOTAS_SCHEMA_NAME,
+  //         localField: 'owner',
+  //         foreignField: 'owner',
+  //         as: 'quota',
+  //       },
+  //     },
+  //     {
+  //       $match: {
+  //         available: {
+  //           $gte: 0,
+  //         },
+  //       },
+  //     },
+  //     {
+  //       $sort: {
+  //         updateAt: -1,
+  //       },
+  //     },
+  //     { $limit: LIMIT_WATCH_DOG_TASK },
+  //   ]);
+  //   // .allowDiskUse(true);
+  // }
 }
