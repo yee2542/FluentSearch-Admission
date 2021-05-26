e5ca34d614ad7e774b4a35778f54eec242d26ad2
yee2542 <yee2542gtl@gmail.com>
2021-05-26 20:49:33 +0700

    add dequeue and upqueue

--- src/quota/quota.service.ts
+++ src/quota/quota.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { QuotaDocument, QUOTAS_SCHEMA_NAME } from 'fluentsearch-types';
 import { Model } from 'mongoose';
@@ -18,6 +18,29 @@ export class QuotaService {
   }
 
   async getUserQuota(owner: string) {
-    return this.quotaModel.findOne({ owner }).lean();
+    return this.quotaModel.findOne({ owner });
+  }
+
+  async deQuota(owner: string) {
+    const userQuota = await this.getUserQuota(owner);
+    if (!userQuota) {
+      Logger.error('User quota not exist');
+      return false;
+    }
+    const valid = userQuota?.available > 0;
+    if (!valid) return false;
+    userQuota.available -= 1;
+    await userQuota.save();
+    return true;
+  }
+
+  async upQuota(owner: string) {
+    const userQuota = await this.getUserQuota(owner);
+    if (!userQuota) {
+      Logger.error('User quota not exist');
+      return;
+    }
+    userQuota.available += 1;
+    return;
   }
 }
