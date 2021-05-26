e1fa393ea046831d06f41dacab03ef79300cf3e0
yee2542 <yee2542gtl@gmail.com>
2021-05-26 20:53:38 +0700

    import need module

--- src/watchdog/watchdog.module.ts
+++ src/watchdog/watchdog.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { QuotaModule } from 'src/quota/quota.module';
+import { TaskModule } from 'src/task/task.module';
 import { WatchdogService } from './watchdog.service';
 
 @Module({
-  providers: [WatchdogService]
+  imports: [TaskModule, QuotaModule],
+  providers: [WatchdogService],
 })
 export class WatchdogModule {}
